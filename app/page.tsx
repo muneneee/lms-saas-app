72e90d609c17bc97852a362576ff2941a53d1f44
kevin kimathi <lukekevin39@gmail.com>
2025-10-08 09:21:20 +0300

    Implement companion card

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,45 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>WELCOME TO SAUTI LMS</h1>
-      <Button> Lets Get Started!!</Button>
-    </div>
+    <main>
+      <h1 className='text-2xl underline'>Popular Companions</h1>
+        <section className='home-section'>
+          <CompanionCard
+            id="12"
+            name="Mbosso the Bongo Explorer"
+            topic="Essence of the Brain"
+            subject="Science"
+            duration={45}
+            color="#1E3A8A"
+          />
+          <CompanionCard
+            id="123"
+            name="Mbosso Explorer"
+            topic="Bongo"
+            subject="Music"
+            duration={35}
+            color="#22C55E"
+          />
+          <CompanionCard
+            id="124"
+            name="Bongo Explorer"
+            topic="Essence"
+            subject="Geography"
+            duration={25}
+            color="#F97316"
+          />
+        </section>
+
+        <section className='home-section'>
+          <CompanionsList/>
+          <CTA/>
+        </section>
+    </main>
   )
 }
 
