60f0f1dea7a49356a1617aaa0dc30ddc10234f58
kevin kimathi <lukekevin39@gmail.com>
2025-10-10 21:34:56 +0300

    Impelement session history

--- app/page.tsx
+++ app/page.tsx
@@ -3,43 +3,33 @@ import CompanionsList from '@/components/CompanionsList'
 import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import { recentSessions } from '@/constants'
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.actions'
+import { getSubjectColor } from '@/lib/utils'
 import React from 'react'
 
-const Page = () => {
+const Page = async () => {
+
+  const companions = await getAllCompanions({ limit: 3});
+  const recentSessionsCompanions = await getRecentSessions(10);
+
   return (
     <main>
       <h1 className='text-2xl underline'>Popular Companions</h1>
         <section className='home-section'>
-          <CompanionCard
-            id="12"
-            name="Mbosso the Bongo Explorer"
-            topic="Essence of the Brain"
-            subject="Science"
-            duration={45}
-            color="#1E3A8A"
-          />
-          <CompanionCard
-            id="123"
-            name="Mbosso Explorer"
-            topic="Bongo"
-            subject="Music"
-            duration={35}
-            color="#22C55E"
-          />
-          <CompanionCard
-            id="124"
-            name="Bongo Explorer"
-            topic="Essence"
-            subject="Geography"
-            duration={25}
-            color="#F97316"
-          />
+          {companions.map((companion) => (
+            <CompanionCard
+              key={companion.id}
+              {...companion}
+              color={getSubjectColor(companion.subject)}
+            />
+          ))}
+        
         </section>
 
         <section className='home-section'>
           <CompanionsList
             title="Recently completed Sessions"
-            companions={recentSessions}
+            companions={recentSessionsCompanions}
             className="w-2/3 max-lg:w-full"
           />
           <CTA/>
